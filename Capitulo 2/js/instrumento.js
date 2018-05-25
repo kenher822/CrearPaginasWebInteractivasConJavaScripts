0a63d85db24dc8fa66408dd13200bbd6dd1cc9f7
Kennett Herrera <kenher822@gmail.com>
2018-05-25 11:19:18 -0600

    Practica_Instrumento
    Se agregaron archivos de la practica numero 2

--- Capitulo 2/js/instrumento.js
+++ Capitulo 2/js/instrumento.js
@@ -1,6 +1,27 @@
+
+// Mostrar atributo href
+var listaEnlaces = document.getElementsByTagName("a");
 var cantidadEnlaces = document.querySelectorAll("a").length;
 
-console.log(cantidadEnlaces);
+function enlacesWeb() {
+    if (cantidadEnlaces > 0){
+        console.log("Hay una cantidad de " + cantidadEnlaces + " enlaces en la página.");
+        console.log("Este es el primer enlace: " + listaEnlaces[0].href);
+        console.log("Este es el último enlace: " + listaEnlaces[cantidadEnlaces - 1].href);
+    }
+}
 
+// Verificar que un elemento contenga la clase
+function contieneClase(id, clase){
+    var instrumento = document.getElementById(id);
+    if (instrumento != null){
+        console.log(instrumento.classList.contains(clase));
+    }else{
+        console.log("No existe el elemento con ese instrumento(" + id + ")");
+    }
+}
 
-var primerEnlace = document.querySelector("a").getAttribute("href")
+contieneClase("saxofon", "madera"); // Muestra true
+contieneClase("saxofon", "metal"); // Muestra false
+contieneClase("trompeta", "metal"); // Muestra true
+contieneClase("contrabajo", "cuerda"); // Muestra "Ningún elemento posee el id contrabajo"
