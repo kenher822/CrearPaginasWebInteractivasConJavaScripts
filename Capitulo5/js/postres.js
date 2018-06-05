1d11e1ae0c00a41edcd5215ef4a3d29bed947f1f
Kennett Herrera <kenher822@gmail.com>
2018-06-05 14:39:00 -0600

    Se realizaron cambios en Practica 3 Capitulo 4
    Se realizaron cambios en Practica 3 Capitulo 4 con eventos de crear elementos nuevos en lista y realizar cambios en elementos previamente creados

--- Capitulo5/js/postres.js
+++ Capitulo5/js/postres.js
@@ -1,14 +1,30 @@
 var boton = document.querySelector("button")
 contador = 1;
 
-function agregarPostre(){
+/* 
+function editarNombre(idPostre) {
+    var nuevoNombre = prompt("Introduzca el nombre a cambiar:");
+    var elemPostres = document.createElement("li");
+    elemPostres.id = document.getElementById("postres").id + String("_" + contador);
+    elemPostres.textContent = nuevoNombre;
+    elemPostres.replaceChild(elemPostres, document.getElementById(idPostre));
+    contador += 1;
+};
+*/
+
+
+boton.addEventListener("click", function () {
     var nuevoPostre = prompt("Ingrese el nuevo postre:");
     idNuevoPostre = document.getElementById("postres").id + String("_" + contador);
-    contador += 1;
     var elemPostres = document.createElement("li");
     elemPostres.id = idNuevoPostre;
     elemPostres.textContent = nuevoPostre;
     document.getElementById("postres").appendChild(elemPostres);
-};
 
-boton.addEventListener("click", agregarPostre);
+    // bonus
+    elemPostres.addEventListener("click", function(e){
+        var nuevoNombre = prompt("Introduzca el nombre a cambiar: " + e.target.textContent);
+        e.target.textContent = nuevoNombre;
+    });
+    contador += 1;
+});
