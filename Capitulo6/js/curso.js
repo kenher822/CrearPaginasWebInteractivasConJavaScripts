00d5c99b6e682bb24addd4ce01ee99b83e4e64bc
Kennett Herrera <kenher822@gmail.com>
2018-06-08 10:31:13 -0600

    Agregando funcionalidades JS
    Se agregaron funcioalidades JS para manipulacion de objetos DOM por medio de eventos

--- Capitulo6/js/curso.js
+++ Capitulo6/js/curso.js
@@ -1,12 +1,28 @@
 var nombreELt = document.getElementById("nombre");
 
 // Muestra un mensaje de texto para ayuda al usuario a introducir un nombre
-nombreELt.addEventListener("focus",function(){
+nombreELt.addEventListener("focus", function () {
     document.getElementById("ayudaNombre").textContent = "Intruduzca un nombre de usuario";
 });
 
 // Borra el mensaje de texto de ayuda para el campo nombre-usuario
-nombreELt.addEventListener("blur", function(){
+nombreELt.addEventListener("blur", function () {
     document.getElementById("ayudaNombre").textContent = "";
 });
+
+// Muestra si el usuario hace click en la casilla confirmación
+document.getElementById("confirmacion").addEventListener("change", function (e) {
+    console.log("Enviar email de confirmación: " + e.target.checked);
+});
+
+// Muestra el tipo de suscripción elegida
+document.getElementsByName("suscripcion").forEach(function (element) {
+    element.addEventListener("change", function (event) {
+        console.log("Suscripción elegida: " + event.target.value);
+    });
+});
+
+
+
+
 
